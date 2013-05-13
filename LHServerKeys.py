4c2ac2af9b18dc7bb8e599035c9bdb694b05daab
James Barrow <baza207@me.com>
2013-05-13 19:31:19 +0200

    Added LHServerKeys and config.txt template.

---
+++ LHServerKeys.py
@@ -0,0 +1,26 @@
+# Defenitions
+TIMESTAMP_FORMAT	= '%d/%m/%Y %H:%M:%S'
+
+# Keys
+kCommand		= 'command'
+kData				= 'data'
+kUUID				= 'UUID'
+
+kLogin				= 'login'
+kLogout			= 'logout'
+kLoginResponse	= 'loginResponse'
+kLogoutResponse	= 'logoutResponse'
+kUserList			= 'userList'
+kChatBroadcast	= 'chatBroadcast'
+kStartType			= 'startType'
+kEndType			= 'endType'
+
+kUsername			= 'username'
+kPassword			= 'password'
+kClientCount		= 'clientCount'
+kChatType			= 'chatType'
+kGroupChat		= 'groupChat'
+kServerChat		= 'serverChat'
+kPrivateChat		= 'privateChat'
+kChat				= 'chat'
+kTimestamp		= 'timestamp'
